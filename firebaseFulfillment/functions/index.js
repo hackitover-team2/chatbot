7c4020b74ae581e20fb101323ce83cc4b2800b7f
Julian Krenge <julian@parcellab.com>
2018-04-28 23:25:13 +0200

    Chatbot kennt das Leaderboard 🥇

--- firebaseFulfillment/functions/index.js
+++ firebaseFulfillment/functions/index.js
@@ -10,6 +10,12 @@ process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
 // Intents
 
+function fallback(agent) {
+
+  agent.add(`Sorry, ich weiß gerade nicht was genau du suchst. Könntest du das noch mal anders formulieren?`);
+
+}
+
 function challenges(agent) {
 
   var options = {
@@ -38,7 +44,33 @@ function challenges(agent) {
     })
     .catch(function (err) {
       console.log(err);
-      agent.add(`Sorry, ich finde die Liste gerade nicht... probier es doch später noch mal, okay?`);
+      agent.add(`Sorry, ich finde die Liste der Challenges gerade nicht... probier es doch später noch mal, okay?`);
+    });
+}
+
+function leaderboard(agent) {
+
+  var options = {
+    method: 'GET',
+    url: 'http://52.57.63.176/leaderboard',
+    headers: { 'Cache-Control': 'no-cache', 'Content-Type': 'application/json' }
+  };
+
+  return request(options)
+    .then(function (body) {
+
+      var leaders = JSON.parse(body);
+
+      var text = leaders.slice(0, 2).map(function (leader, index) {
+        return `${ ['🥇', '🥈', '🥉'][index]} — ${leader.firstName} ${leader.lastName.charAt(0)}.\nhat ${leader.wonChallenges.length} Badge(s) und nimmt an ${leader.participatingChallenges.length} Challenge(s) teil!`;
+      }).join('\n\n');
+
+      agent.add(text);
+
+    })
+    .catch(function (err) {
+      console.log(err);
+      agent.add(`Sorry, ich finde die Bestenliste gerade nicht... probier es doch später noch mal, okay?`);
     });
 }
 
@@ -50,5 +82,9 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   // console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   // console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-  agent.handleRequest(challenges);
+  var handler = fallback;
+  if (request.body.queryResult.intent.displayName === 'Challenges') handler = challenges;
+  if (request.body.queryResult.intent.displayName === 'Leaderboard') handler = leaderboard;
+
+  agent.handleRequest(handler);
 });
